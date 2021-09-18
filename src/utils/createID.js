b6913585eeb2e79e0ea0efb49913df489880a4cb
Harrison Howard <harrison.howard00707@gmail.com>
2021-09-18 15:05:04 +1000

    createID util

---
+++ src/utils/createID.js
@@ -0,0 +1,15 @@
+const cryptojs = require("crypto-js");
+
+/**
+ * Create a random ID
+ * @param {"SHA1" | "SHA224" | "SHA256" | "SHA3" | "SHA384" | "SHA512"} [sha=] SHA256 is default
+ * @returns {string}
+ */
+module.exports = (sha = "SHA256") => {
+    if (
+        !["SHA1", "SHA224", "SHA256", "SHA3", "SHA384", "SHA512"].includes(sha)
+    ) {
+        throw new TypeError("Invalid SHA method");
+    }
+    return cryptojs[sha](str).toString();
+};
