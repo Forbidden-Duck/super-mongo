7a998f6e48ac243bdd9731d91175f16ec0c4eeb0
Harrison Howard <harrison.howard00707@gmail.com>
2021-09-18 12:30:10 +1000

    Mongo data types, including BSON

---
+++ src/datatypes/index.js
@@ -0,0 +1,43 @@
+const bson = require("bson");
+
+module.exports.Array = Array;
+
+module.exports.Binary = bson.Binary;
+
+module.exports.Boolean = Boolean;
+
+module.exports.BSONRegExp = bson.BSONRegExp;
+
+module.exports.BSONSymbol = bson.BSONSymbol;
+
+module.exports.BSONMap = bson.Map;
+
+module.exports.Code = bson.Code;
+
+module.exports.Date = Date;
+
+module.exports.Decimal128 = bson.Decimal128;
+
+module.exports.Double = bson.Double;
+
+module.exports.Int32 = bson.Int32;
+
+module.exports.Int64 = Number;
+
+module.exports.Long = bson.Long;
+
+module.exports.MaxKey = bson.MaxKey;
+
+module.exports.MinKey = bson.MinKey;
+
+module.exports.Null = null;
+
+module.exports.Object = Object;
+
+module.exports.ObjectID = bson.ObjectID;
+
+module.exports.String = String;
+
+module.exports.Timestamp = bson.Timestamp;
+
+module.exports.Undefined = undefined;
