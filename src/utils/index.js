de09d7bde72a1882232417b2c1695989050cb88b
Harrison Howard <harrison.howard00707@gmail.com>
2021-09-19 16:12:38 +1000

    Index file for utils

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+module.exports.Base64 = require("./Base64");
+
+module.exports.Hash = require("./Hash");
+
+module.exports.ID = require("./ID");
+
+module.exports.Obj2Schema = require("./Obj2Schema");
+
+module.exports.Token = require("./Token");
