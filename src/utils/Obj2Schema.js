0704bfcbdf42d4e4d7891c78fb555488f7e6c9cf
Harrison Howard <harrison.howard00707@gmail.com>
2021-09-19 19:50:20 +1000

    Improved schema interpretation, now supports multi-dimensonal objects and arrays :D

--- src/utils/Obj2Schema.js
+++ src/utils/Obj2Schema.js
@@ -1,28 +1,22 @@
+const { JSArray, JSObject } = require("../datatypes");
 const Types = require("./Types");
 
 /**
  * Compare and force an obj to follow the schema rules
  * @template {object} T
- * @param {T} obj
+ * @template {T} S
+ * @param {S} obj
  * @param {T} schema
  * @param {{ noUndefined: boolean, noNull: boolean }} [options]
- * @returns
+ * @returns {T}
  */
 module.exports.compare = (obj, schema, options) => {
     options = typeof options === "object" ? options : {};
     const data = Object.assign({}, obj);
     for (const [key, value] of Object.entries(data)) {
-        if (
-            !schema[key] ||
-            (!(data[key] instanceof schema[key]) &&
-                !Types.checkType2Primitives(data[key], schema[key]) &&
-                ![null, undefined].includes(data[key])) ||
-            (data[key] === undefined && options.noUndefined) ||
-            (data[key] === null && options.noNull)
-        ) {
-            delete data[key];
-        }
+        if (!this.compareOne(value, schema[key], options)) delete data[key];
     }
+    // Add undefined to missing properties if noUndefined is true
     if (!options.noUndefined) {
         for (const key of Object.keys(schema).filter(
             (item) => data[item] === undefined
@@ -30,5 +24,58 @@ module.exports.compare = (obj, schema, options) => {
             data[key] = undefined;
         }
     }
+    // Run schema compare on inner object and array properties
+    for (const [key, value] of Object.entries(data)) {
+        if (Types.checkType2Primitives(value, JSObject)) {
+            data[key] = this.compare(value, schema[key], options);
+        } else if (
+            Types.checkType2Primitives(value, JSArray) &&
+            Types.checkType2Primitives(schema[key], JSArray)
+        ) {
+            data[key] = this.compareArray(value, schema[key][0], options);
+        }
+    }
     return data;
+};
+
+module.exports.compareOne = (value, schemaType, options) => {
+    options = typeof options === "object" ? options : {};
+    function checkInstanceOf(v, s) {
+        if (
+            !Types.checkType2Primitives(v, JSObject) &&
+            !Types.checkType2Primitives(s, JSObject) &&
+            !Types.checkType2Primitives(s, JSArray)
+        ) {
+            return v instanceof s;
+        }
+        return false;
+    }
+    return !(
+        !schemaType ||
+        (!checkInstanceOf(value, schemaType) && // Check instance and primitives (if one works then continue)
+            !Types.checkType2Primitives(value, schemaType) &&
+            ![null, undefined].includes(value)) || // Ignore null and undefined
+        (value === undefined && options.noUndefined) ||
+        (value === null && options.noNull)
+    );
+};
+
+module.exports.compareArray = (arr, schemaType, options) => {
+    options = typeof options === "object" ? options : {};
+    const data = [...arr];
+    const index2Delete = [];
+    data.forEach((value, index) => {
+        if (Types.checkType2Primitives(schemaType, JSObject)) {
+            if (Types.checkType2Primitives(value, JSObject)) {
+                data[index] = this.compare(value, schemaType, options);
+            } else index2Delete.push(index);
+        } else if (Types.checkType2Primitives(schemaType, JSArray)) {
+            if (Types.checkType2Primitives(value, JSArray)) {
+                data[index] = this.compareArray(value, schemaType[0], options);
+            } else index2Delete.push(index);
+        } else if (!this.compareOne(value, schemaType, options)) {
+            index2Delete.push(index);
+        }
+    });
+    return data.filter((value, index) => !index2Delete.includes(index));
 };
