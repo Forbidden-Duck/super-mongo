2022aebaeaa7483bfa50ad64055093526f4c7eec
Harrison Howard <harrison.howard00707@gmail.com>
2021-09-21 12:12:58 +1000

    fix index typings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 "use strict";
 const MongoClient = require("./MongoClient");
+const Database = require("./structures/Database");
+const MongoDB = require("mongodb");
+
+/**
+ * @typedef {object} ClientConnection
+ * @property {string} address represents the address (i.e. localhost:27017)
+ * @property {string} [host] represents the host (where the user auths to) (i.e. mydatabase)
+ * @property {string} [username] login username
+ * @property {string} [password] login password
+ */
 
 /**
  *
